dbf15d896359644107d8f75d36598bc06a42a82a
hitao123 <hitao1234@gmail.com>
2018-11-25 23:47:09 +0800

    fix(module): package module name pascal

--- packages/utils/create.js
+++ packages/utils/create.js
@@ -2,7 +2,7 @@ import createBasic from './create-basic';
 import Loading from '../loading';
 import Icon from '../icon';
 import Cell from '../cell';
-import CellGroup from '../cellgroup';
+import CellGroup from '../cell-group';
 
 export default function (opt) {
   opt.components = Object.assign(opt.components || {}, {}, {
