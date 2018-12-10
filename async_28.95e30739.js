0d89f4b520a61a81d480ff80ee3f87404796e081
hitao123 <hitao1234@gmail.com>
2018-12-10 23:37:00 +0800

    Updates

---
+++ async_28.95e30739.js
@@ -0,0 +1,98 @@
+webpackJsonp([28],{
+
+/***/ 219:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_index_vue__ = __webpack_require__(440);
+/* empty harmony namespace reexport */
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_42dca7fa_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_index_vue__ = __webpack_require__(560);
+var normalizeComponent = __webpack_require__(0)
+/* script */
+
+
+/* template */
+
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_index_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_42dca7fa_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_index_vue__["a" /* default */],
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+
+/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
+
+
+/***/ }),
+
+/***/ 440:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["a"] = ({
+  data: function data() {
+    return {
+      checked: false,
+      checked2: true,
+      checked3: true,
+      checked4: true
+    };
+  },
+
+  methods: {
+    handleInput: function handleInput(value) {
+      var _this = this;
+
+      this.$dialog.confirm({
+        title: '提醒',
+        message: '是否切换开关'
+      }).then(function () {
+        _this.checked4 = value;
+      });
+    }
+  }
+});
+
+/***/ }),
+
+/***/ 560:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('demo-section',[_c('demo-block',{attrs:{"title":"基础用法"}},[_c('h-switch',{model:{value:(_vm.checked),callback:function ($$v) {_vm.checked=$$v},expression:"checked"}})],1),_vm._v(" "),_c('demo-block',{attrs:{"title":"disabled 状态"}},[_c('h-switch',{attrs:{"disabled":""},model:{value:(_vm.checked2),callback:function ($$v) {_vm.checked2=$$v},expression:"checked2"}})],1),_vm._v(" "),_c('demo-block',{attrs:{"title":"loading 状态"}},[_c('h-switch',{attrs:{"loading":""},model:{value:(_vm.checked3),callback:function ($$v) {_vm.checked3=$$v},expression:"checked3"}})],1),_vm._v(" "),_c('demo-block',{attrs:{"title":"高级用法"}},[_c('h-switch',{attrs:{"value":_vm.checked4},on:{"input":_vm.handleInput}})],1)],1)}
+var staticRenderFns = []
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
+
+/***/ })
+
+});
---
+++ async_66.46269cf9.js
@@ -0,0 +1,68 @@
+webpackJsonp([66],{
+
+/***/ 156:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_node_modules_fast_vue_md_loader_lib_index_js_intro_README_md__ = __webpack_require__(378);
+/* empty harmony namespace reexport */
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_41f62726_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_node_modules_fast_vue_md_loader_lib_index_js_intro_README_md__ = __webpack_require__(447);
+var normalizeComponent = __webpack_require__(0)
+/* script */
+
+
+/* template */
+
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __WEBPACK_IMPORTED_MODULE_0__babel_loader_node_modules_vue_loader_lib_selector_type_script_index_0_node_modules_fast_vue_md_loader_lib_index_js_intro_README_md__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_41f62726_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_node_modules_fast_vue_md_loader_lib_index_js_intro_README_md__["a" /* default */],
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+
+/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
+
+
+/***/ }),
+
+/***/ 378:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["a"] = ({
+  created: function created() {
+    this.content = unescape("%3Cdiv%20class%3D%22h-doc-intro%22%3E%0A%20%20%3Ch2%3EHui%3C/h2%3E%0A%20%20%3Cp%3E%u8F7B%u91CF%u3001%u53EF%u9760%u7684%u79FB%u52A8%u7AEF%20Vue%20%u7EC4%u4EF6%u5E93%3C/p%3E%0A%3C/div%3E%0A%3Ch3%3E%u7279%u6027%3C/h3%3E%0A%3Cul%3E%0A%3Cli%3E40+%u7EC4%u4EF6%3C/li%3E%0A%3Cli%3E%u5355%u5143%u6D4B%u8BD5%u8986%u76D6%u7387%u8D85%u8FC7%2090%25%3C/li%3E%0A%3Cli%3E%u5B8C%u5584%u7684%u6587%u6863%u548C%u793A%u4F8B%3C/li%3E%0A%3Cli%3E%u652F%u6301%20%3Ca%20href%3D%22https%3A//github.com/ant-design/babel-plugin-import%22%3Ebabel-plugin-import%3C/a%3E%3C/li%3E%0A%3Cli%3E%u652F%u6301%20SSR%3C/li%3E%0A%3C/ul%3E%0A%3Ch3%3E%u5FEB%u901F%u4E0A%u624B%3C/h3%3E%0A%3Cp%3E%u8BF7%u53C2%u8003%20%3Ca%20href%3D%22%23/quickstart%22%3E%u5FEB%u901F%u4E0A%u624B%3C/a%3E%3C/p%3E%0A%3Ch3%3E%u8D21%u732E%u4EE3%u7801%3C/h3%3E%0A%3Cp%3E%u4FEE%u6539%u4EE3%u7801%u8BF7%u9605%u8BFB%u6211%u4EEC%u7684%20%3Ca%20href%3D%22%23/contribution%22%3E%u5F00%u53D1%u6307%u5357%3C/a%3E%3C/p%3E%0A%3Cp%3E%u4F7F%u7528%u8FC7%u7A0B%u4E2D%u53D1%u73B0%u4EFB%u4F55%u95EE%u9898%u90FD%u53EF%u4EE5%u63D0%20%3Ca%20href%3D%22https%3A//github.com/hitao123/hui/issues%22%3EIssue%3C/a%3E%20%u7ED9%u6211%u4EEC%uFF0C%u5F53%u7136%uFF0C%u6211%u4EEC%u4E5F%u975E%u5E38%u6B22%u8FCE%u4F60%u7ED9%u6211%u4EEC%u53D1%20%3Ca%20href%3D%22https%3A//github.com/hitao123/hui/pulls%22%3EPR%3C/a%3E%3C/p%3E%0A%3Ch3%3E%u6D4F%u89C8%u5668%u652F%u6301%3C/h3%3E%0A%3Cp%3E%u73B0%u4EE3%u6D4F%u89C8%u5668%u4EE5%u53CA%20Android%204.0+%2C%20iOS%206+%3C/p%3E%0A%3Ch3%3E%u94FE%u63A5%3C/h3%3E%0A%3Cul%3E%0A%3Cli%3E%3Ca%20href%3D%22https%3A//github.com/hitao123/hui/issues%22%3E%u610F%u89C1%u53CD%u9988%3C/a%3E%3C/li%3E%0A%3Cli%3E%3Ca%20href%3D%22%23/changelog%22%3E%u66F4%u65B0%u65E5%u5FD7%3C/a%3E%3C/li%3E%0A%3Cli%3E%3Ca%20href%3D%22https%3A//hitao123.github.io/hui/%22%3Ehui-demo%3C/a%3E%3C/li%3E%0A%3Cli%3E%3Ca%20href%3D%22https%3A//github.com/rvs-template/vue-cli-template-hui%22%3Evue-cli-template-hui%3C/a%3E%3C/li%3E%0A%3C/ul%3E%0A%3Ch3%3E%u5F00%u6E90%u534F%u8BAE%3C/h3%3E%0A%3Cp%3E%u672C%u9879%u76EE%u57FA%u4E8E%20%3Ca%20href%3D%22https%3A//zh.wikipedia.org/wiki/MIT%25E8%25A8%25B1%25E5%258F%25AF%25E8%25AD%2589%22%3EMIT%3C/a%3E%20%u534F%u8BAE%uFF0C%u8BF7%u81EA%u7531%u5730%u4EAB%u53D7%u548C%u53C2%u4E0E%u5F00%u6E90%3C/p%3E%0A");
+  }
+});
+
+/***/ }),
+
+/***/ 447:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('section',{domProps:{"innerHTML":_vm._s(_vm.content)}})}]
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
+
+/***/ })
+
+});
